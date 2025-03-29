200c0dfd6ce1494659fa229cbc5f67adc1742938
Mikhail Mironov <mihail.borokoko@gmail.com>
2025-03-29 17:35:32 +0300

    added basic tkinter ui

--- src/download.py
+++ src/download.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 from pathlib import Path
 from typing import Dict
 
@@ -14,17 +15,26 @@ async def get_index_link() -> str:
     return index_link
 
 
-def main():
-    # Select film you want to download and pass index_url to Downloader
+def download(save_dir: Path, output_filename: str) -> None:
     configure_logging()
+    logging.info("Starting to download.\nOutput Directory: %s\nFilename: %s" % (str(save_dir), output_filename))
+
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     index_url: str = loop.run_until_complete(get_index_link())
     hrefs: Dict[str, str] = parse_m3u8_file(index_url=index_url)
     gluer: VideoSegmentGluer = VideoSegmentGluer(
-        segment_links=hrefs, save_dir=Path(r"D:\HSE\Introduction_Python\project\src\videos")
+        segment_links=hrefs, save_dir=save_dir
     )
-    gluer.process(output_filename="full_movie.ts", cleanup=True)
+    gluer.process(output_filename=output_filename, cleanup=True)
+
+
+def main():
+    # Select film you want to download and pass index_url to Downloader
+    download(
+        save_dir=Path(r"D:\HSE\Introduction_Python\project\src\videos"),
+        output_filename="movie.ts",
+    )
 
 
 if __name__ == '__main__':
